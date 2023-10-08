5a9f83684fb9fd24769559c948c6a5b0faa13714
Steven Trotter <stevetrot@gmail.com>
2023-10-08 13:33:11 +0100

    Fix

--- lua/strottos/bindings.lua
+++ lua/strottos/bindings.lua
@@ -1,5 +1,5 @@
-vim.api.nvim_set_keymap('v', 'K', ':m '>+1<CR>gv=gv', {noremap = true})
-vim.api.nvim_set_keymap('v', 'J', ':m '<-2<CR>gv=gv', {noremap = true})
+vim.api.nvim_set_keymap('v', 'K', ":m '>+1<CR>gv=gv", {noremap = true})
+vim.api.nvim_set_keymap('v', 'J', ":m '<-2<CR>gv=gv", {noremap = true})
 
 vim.api.nvim_set_keymap('v', '<leader>K', 'K', {noremap = true})
 vim.api.nvim_set_keymap('v', '<C-J>', 'J', {noremap = true})
